d82b5f469a567ed32e8402c55c0b33a94f1bb0e0
Jackandoggy <98306653+Jackandoggy@users.noreply.github.com>
2022-02-10 10:22:10 +0530

    Update translation.py

--- translation.py
+++ translation.py
@@ -7,25 +7,23 @@ class LuciferMoringstar(object):
     ABOUT_MSG = """
 🤖 My Name [{bot_name}](t.me/{bot_username})
 
-🧑 My Dev : [PR0FESS0R-99](t.me/PR0FESS0R_99)
+🧑 My Dev : [Alif Muhammed](t.me/Alifmuhammed_tg)
 
-🧑 My Creator : {dev_name}
+🧑 co editor : [Geronimo ⁪⁬⁮⁮⁮⁮ ‌‌‌‌Lover](t.me/Geronimo1234)
 
 📦 My Source : [Click Here](https://github.com/PR0FESS0R-99/LuciferMoringstar_Robot)
 
-📺 My Tutorial : [Ckick Here](https://youtu.be/FCU_XJmyG_U)
+🗃️ My Library : [Pyrogram](https://docs.pyrogram.org/)
 
-🗃️ My Library : [Pyrogram](https://youtu.be/Af055Eozk9s)
+🖇️ My Language : [Python3](https://www.googleadservices.com/pagead/aclk?sa=L&ai=DChcSEwj9i-n_qvT1AhWLgksFHQcmA2wYABAAGgJzZg&ae=2&dsh=1&agsac=HxvJH1NkuC4&cit=CkQKCAiA6Y2QBhANEjQAVXlHtiX1H53dI8cFcPt-0P-cWnMTmmse0ZwOkdhVnS2ih74CDEzg0uLAzt-AIc4LEqY3GgJymvD_BwE&ohost=www.google.com&cid=CAESWOD25HHr79OcSQcg4OuFZuUwkDSbvxgBu0fh_G9mqX9-NAMiq421NjPwlhNbmbIkdRLfJ4KLu0oNRLr3H5B3US7mWjonb2WPaAg8PNVbQccBDYxgxI2bLz8&sig=AOD64_3DdztCZiOsSbptfgxpwF1Ktjy0ug&q&adurl&ved=2ahUKEwjU8OH_qvT1AhVEyDgGHc8tDTIQ0Qx6BAgEEAE&ms=CoACCdaKg0hyBb37yYKaHByJzAgZ2f3zLAq4Q385M9oA2g494pxowtqfSyc1plir5daRzks0dNz36f8sDybzSnxO-SdI7iI_r2VHY6D9pGBu7_pFZG-498Mn7xv9hsIrm_wIq4PtGSdI_ucmcjkXdZCjjjNKah-VFBxFRVHH6CY6gcoxyH8VlkGIdqAkFg9OmGAyPNnranD9vEK3pPUWThJSstkmnvlQImx0LehzZ0W17P0PkPKSIrT1_wulDNyy6W6I4baSZuoPbwcHN_hiQQVOgWlI1IuXVpg1CdOkXLWLO8YDyzRlWy8sm1qODZ0Hvo4uU0mQ4grZiEDjZSOS9f7-eAqAAnFUvTpKR2kvHsAr_0Pzlnpj1dD554B6F9c6iIeUjdeY3eG81Q51VLxIxIIuZ2hhpTa-iSyeoQCL-EOQKBijgCh6XWcTLJfAXf6nB_j1d_a-7fs6yfMy29fy0ulgxIawDnPKN_IbYCHHDbky03Np_PWaVBgzZysKaV6qwrbe-6Oh7u598ozcFRV8maFazmPrp7ZcJfZ2gXyNGS5Uvbgxm5KpwF78VwLrUPp3CKxmmbaeMX0_H6TzPIabdd1uPxRAPewDNBoq0eL7PYU8hg4R7cFxzd5F-Tf-dNJHKrxtHHzEKYUHeqp3aL0c5an3V65VzrCK4PcJM0OfWSLWw71X5aoSEBOl7VpCVUbDtxBAIvQ_8iw&dct=1)
 
-🖇️ My Language : [Python3](https://youtu.be/Af055Eozk9s)
+🗃️ My Database : [Mongodb](https://www.mongodb.com/cloud/atlas/lp/try2-in?utm_source=google&utm_campaign=gs_apac_india_search_core_brand_atlas_mobile&utm_term=mongodb&utm_medium=cpc_paid_search&utm_ad=e&utm_ad_campaign_id=12564980858&adgroup=116332189621&gclid=CjwKCAiA6Y2QBhAtEiwAGHybPd5ykBAnt6XUeyssBXkkI64rCCHM4PtssrOnc2x3D-3US38W9VbLqRoCF8UQAvD_BwE)
 
-🗃️ My Database : [Mongodb](https://youtu.be/gBLTsH-IXr0)
+🗳️ My Server : [Heroku](https://dashboard.heroku.com/)
 
-🗳️ My Server : [Heroku](https://youtu.be/xcZD55td6Vw)
+🏷️ My Updates : [Movie go down](t.me/film_godown)
 
-🏷️ My Updates : [Mo Tech](t.me/Mo_Tech_YT)
-
-🗣️ My Support : [Any Doubt](t.me/Mo_Tech_Group)"""
+🗣️ My Support : [Team Movie](t.me/movie_go_down)"""
 
     FILE_CAPTIONS = """Hello 👋 {mention}\n\n📁Title {title}\n\n🔘Size {size}"""
 
